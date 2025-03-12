886ff72269c87f41f8934233d7bb5565b6230577
Kaue Sabino <kaue.sabino@modalgr.io>
2025-03-12 11:30:43 -0300

    feat: adicionando tratativa para o CORS da aplicação em angular

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import router
 
 app = FastAPI(title="Minha API Python com JWT e SQL Server")
+
+# Permitir CORS para o frontend Angular
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # Angular local
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os headers
+)
 
 app.include_router(router)
 
